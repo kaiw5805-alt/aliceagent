73a78fec660e94502ba6d94a699337c34731b4b2
Landon <kaiw5805@gmail.com>
2025-08-12 11:37:10 +0800

    feat: assets

--- src/chat.ts
+++ src/chat.ts
@@ -1,10 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,Logger } from '@nestjs/common';
 import { getSDK, CbEvents, WSEvent } from '@openim/wasm-client-sdk';
+import * as path from 'path';
+
+const apiAddr = 'http://127.0.0.1:10002';
+const wsAddr = 'ws://127.0.0.1:10001';
+
+const coreWasmPath = path.resolve(__dirname, '../assets/openim.wasm');
+const sqlWasmPath = path.resolve(__dirname, '../assets/sql-wasm.wasm');
+
+const config = {
+  coreWasmPath: coreWasmPath,
+  sqlWasmPath: sqlWasmPath,
+  debug: true,
+};
 
 @Injectable()
 export class Chat {
-  private readonly OpenIM = getSDK();
+  private readonly OpenIM = getSDK(config);
+  private readonly logger = new Logger(Chat.name);
   constructor() {
+    this.logger.log('Chat constructor', coreWasmPath);
     this.OpenIM.on(CbEvents.OnConnecting, this.handleConnecting);
     this.OpenIM.on(CbEvents.OnConnectFailed, this.handleConnectFailed);
     this.OpenIM.on(CbEvents.OnConnectSuccess, this.handleConnectSuccess);
