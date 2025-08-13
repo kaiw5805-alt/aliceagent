713bafc97c3e936ca69fe122b8218ceb6a321fcb
Landon <kaiw5805@gmail.com>
2025-08-13 10:00:02 +0800

    feat: 去掉chat

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppBot } from './app.bot';
-import { Chat } from './chat';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, AppBot, Chat],
+  providers: [AppService, AppBot],
 })
 export class AppModule {}
